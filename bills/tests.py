5f421d9d55aa419cbab66f34f9dabd2ce6e18226
Ben Selby <benmatselby@gmail.com>
2024-12-20 21:00:04 +0000

    Add in a GitHub workflow

--- bills/tests.py
+++ bills/tests.py
@@ -7,3 +7,18 @@ class TestType(TestCase):
     def test_str(self):
         type = Type(handle="electricity", description="Electricity")
         self.assertEqual(str(type), "electricity")
+
+
+class TestBill(TestCase):
+    def test_str(self):
+        type = Type(handle="electricity", description="Electricity")
+        bill = Bill(
+            type=type,
+            date_start="2024-12-01 00:00:00",
+            date_end="2024-12-31 23:59:59",
+            cost=1000,
+        )
+
+        self.assertEqual(
+            str(bill), "electricity - 2024-12-01 00:00:00 - 2024-12-31 23:59:59 - 1000"
+        )
